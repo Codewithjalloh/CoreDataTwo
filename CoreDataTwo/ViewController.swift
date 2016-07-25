60a7e980dffadb733b7c59a97a95bc36885d522c
D.A Jalloh <info@codewithjalloh.com>
2016-07-25 20:43:33 +0100

    code complete

--- CoreDataTwo/ViewController.swift
+++ CoreDataTwo/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import CoreData
 
 class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     
@@ -20,7 +21,7 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        self.loadTable()
     }
 
     override func didReceiveMemoryWarning() {
@@ -30,7 +31,46 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
     
     @IBAction func save(sender: UIButton) {
         let appDel: AppDelegate = UIApplication.sharedApplication().delegate  as! AppDelegate
+        let context: NSManagedObjectContext = appDel.managedObjectContext
+        let cell = NSEntityDescription.insertNewObjectForEntityForName("Form", inManagedObjectContext: context)
+        cell.setValue(name, forKey: "name")
+        cell.setValue(surname, forKey: "surname")
         
+        do {
+            try context.save()
+        } catch {
+            
+        }
+        
+        self.loadTable()
+        self.table.reloadData()
+
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return results!.count
+    }
+    
+    func tableView(tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return "Contacts"
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell: UITableViewCell = UITableViewCell(style: .Subtitle, reuseIdentifier: nil)
+        let aux = results![indexPath.row] as! NSManagedObject
+        cell.textLabel!.text = aux.valueForKey("name") as? String
+        cell.detailTextLabel!.text = aux.valueForKey("surname") as? String
+        
+        return cell
+    }
+    
+    
+    func loadTable() {
+        let appDel: AppDelegate = UIApplication.sharedApplication().delegate as! AppDelegate
+        let context:NSManagedObjectContext = appDel.managedObjectContext
+        let request = NSFetchRequest(entityName: "Form")
+        request.returnsObjectsAsFaults = false
+        results = try? context.executeFetchRequest(request)
         
     }
 
